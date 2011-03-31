b4beed36a1ccd3ab41b60e7d08fd605fa15662e6
lyudmil.pelov <lyudmil.pelov@9a5825ca-b633-a590-24fc-95245d667661>
2011-03-31 17:59:06 +0000

    code review

--- trunk/MVP2.2/src/com/mvp/client/ui/widget/SimpleWidgetPanel.java
+++ trunk/MVP2.2/src/com/mvp/client/ui/widget/SimpleWidgetPanel.java
@@ -3,19 +3,35 @@ package com.mvp.client.ui.widget;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Custom widget implementing the HasOneWidget interface.
+ * 
  * @author L.Pelov
  */
 public class SimpleWidgetPanel extends LayoutPanel implements AcceptsOneWidget {
 
-    private IsWidget widget = null;
-    
-    @Override
-    public void setWidget(IsWidget w) {
-        if( widget != null) super.remove(widget);
-        widget = w;
-        if(w != null) super.add(w);
-    }
+	private IsWidget IsWidget = null;
+
+	@Override
+	public void setWidget(IsWidget w) {
+
+		if (w == IsWidget) {
+			return;
+		}
+
+		Widget widget = asWidgetOrNull(w);
+		
+		if (widget != null) {
+			super.remove(widget);
+		}
+
+		IsWidget = widget;
+
+		if (widget != null) {
+			super.add(widget);
+		}
+
+	}
 }
