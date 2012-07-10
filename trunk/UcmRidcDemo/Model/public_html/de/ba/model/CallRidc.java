cffba875ee6ff97986864cbee3b676a31b1017aa
lyudmil.pelov@gmail.com <lyudmil.pelov@gmail.com@9a5825ca-b633-a590-24fc-95245d667661>
2012-07-10 11:35:55 +0000

    Example shows how to programmaticaly connect to UCM via RIDC using the Oracle UCM Native Web Services

---
+++ trunk/UcmRidcDemo/Model/public_html/de/ba/model/CallRidc.java
@@ -0,0 +1,71 @@
+package de.ba.model;
+
+import oracle.stellent.ridc.IdcClient;
+import oracle.stellent.ridc.IdcClientException;
+import oracle.stellent.ridc.IdcClientManager;
+import oracle.stellent.ridc.IdcContext;
+import oracle.stellent.ridc.model.DataBinder;
+import oracle.stellent.ridc.model.DataObject;
+import oracle.stellent.ridc.model.DataResultSet;
+import oracle.stellent.ridc.protocol.ServiceResponse;
+import oracle.stellent.ridc.protocol.jaxws.JaxWSClient;
+import oracle.stellent.ridc.protocol.jaxws.JaxWSClientConfig;
+
+public class CallRidc {
+    private String ridcName = "Ridc UCM";
+    
+    public CallRidc() {        
+    }
+
+    private void createConnection() throws IdcClientException {
+        
+        // create the manager
+        IdcClientManager manager = new IdcClientManager();
+
+        // build a client that will communicate using the JAXWS protocol
+        JaxWSClient idcClient = (JaxWSClient)manager.createClient("http://l6899022.sdst.sbaintern.de:16200/idcnativews");
+        
+        // get the config object and set properties
+        idcClient.getConfig ().setSocketTimeout (30000); // 30 seconds
+        idcClient.getConfig ().setConnectionSize (20);   // 20 connections
+
+        JaxWSClientConfig jaxwsConfig = idcClient.getConfig();
+
+        // set the property
+        jaxwsConfig.setServerInstanceName("/cs/");
+        
+        // create the user context
+        IdcContext idcContext = new IdcContext ("weblogic", "Weblogic1");
+
+        // build the search request binder
+        DataBinder binder = idcClient.createBinder();
+        binder.putLocal("IdcService", "GET_SEARCH_RESULTS");
+        binder.putLocal("QueryText", "");
+        binder.putLocal("ResultCount", "20");
+
+        // send the initial request
+        ServiceResponse response = idcClient.sendRequest(idcContext, binder);
+        DataBinder responseBinder = response.getResponseAsBinder();
+
+        DataResultSet resultSet = responseBinder.getResultSet ("SearchResults");
+
+        // loop over the results
+        for (DataObject dataObject : resultSet.getRows ()) {
+            System.out.println ("Title is: " + dataObject.get ("dDocTitle"));
+            System.out.println ("Author is: " + dataObject.get ("dDocAuthor"));
+        }
+
+
+
+    }
+    
+    
+    public String getRidcName() {
+        try {
+            createConnection();
+        } catch (IdcClientException e) {
+            return "Exception: " + e.getMessage();
+        }
+        return ridcName;
+    }
+}
